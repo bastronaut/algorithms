07ca6e9c21fd90fd90ec7f5ef557264a80451e9f
bassijtsma <btsijtsma@gmail.com>
2016-06-26 23:34:56 +0200

    Setup java project

---
+++ algorithms/src/BinarySearch.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Bas on 26-6-2016.
+ */
+public class BinarySearch {
+//    returns the index of the word to be searched in an ordered list. Returns -1 if not in list
+    public static int Search(String word, int[] orderedList) {
+        return 0;
+    }
+
+    public static int Search(String word, int[] orderedList, int high, int low) {
+        int mid = ((high - low) / 2) + low;
+        if (TODO) {
+
+        }
+    }
+}
---
+++ algorithms/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Bas on 25-6-2016.
+ */
+class Main {
+    public static void main(String args[]){
+        System.out.println("main");
+    }
+}

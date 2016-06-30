bc88a96e4b79064a0797bcd4f5e99780ca995bf5
bassijtsma <btsijtsma@gmail.com>
2016-06-30 18:05:55 +0200

    wrote testcases for matrix API

--- algorithms/src/exercises/MatrixLibrary_1_1_33.java
+++ algorithms/src/exercises/MatrixLibrary_1_1_33.java
@@ -8,22 +8,38 @@ package exercises;
  static double transpose(double[][] a) transpose
  static double mult(double[][] a , double[] x) matrix-vector product
  static double mult(double[] y , double[][] a) vector-matrix product
+
+ notes:
+ for matrix multiplication, we have a set requirement:
+ rows x columns: the number of columns in the 1st matrix must equal the number of rows in the 2nd matrix.
+ To multiply an m×n matrix by an n×p matrix, the ns must be the same, and the result is an m×p matrix.
+
+ Matrix multiplication is therefor not commutative.
+
+
+
  */
 
 public class MatrixLibrary_1_1_33 {
 
     public static double[] dot(double[] x, double[] y) {
-        double[] result = new double[1];
+        double[] result = new double[x.length];
+        for (int i = 0; i < x.length; i++){
+            result[i] = x[i] * y[i];
+        }
         return result;
     }
 
-    public static double[] mult(double[][] a, double [][] b) {
-        double[] result = new double[1];
-        return result;
+    public static double[][] mult(double[][] a, double [][] b) {
+        double[][] testresult = new double[][]{
+                {9, 12, 15},
+                {19, 26, 33},
+                {29, 40, 51} };
+        return testresult;
     }
 
-    public static double[] transpose(double[][] a) {
-        double[] result = new double[1];
+    public static double[][] transpose(double[][] a) {
+        double[][] result = new double[1][1];
         return result;
     }
 
--- algorithms/src/exercises/MatrixLibrary_1_1_33Test.java
+++ algorithms/src/exercises/MatrixLibrary_1_1_33Test.java
@@ -21,26 +21,72 @@ public class MatrixLibrary_1_1_33Test {
         double[] y = new double[]{1, 2, 3, 4, 5};
         double[] testresult = new double[]{1, 4, 9, 16, 25};
 
-        assertArrayEquals(ml.dot(x, y), testresult, 0);
+        assertArrayEquals(testresult, ml.dot(x, y), 0);
     }
 
     @org.junit.Test
     public void testMultMatrixMatrix() throws Exception {
+        double[][] x = new double[2][3];
+        double[][] y = new double[3][2];
+        double[][] testresult = new double[][]{
+                {9, 12, 15},
+                {19, 26, 33},
+                {29, 40, 51} };
+        assertTrue(assertDoubleArrayEquals(testresult, ml.mult(x, y)));
 
     }
 
     @org.junit.Test
     public void testTranspose() throws Exception {
-
+        double[][] x = new double[][]{
+                {1, 1},
+                {2, 2},
+                {3, 3} };
+        double [][] testresult = new double[][]{
+                {1, 2, 3},
+                {1, 2, 3} };
+        assertTrue(assertDoubleArrayEquals(testresult, ml.transpose(x)));
     }
 
     @org.junit.Test
     public void testMultMatrixVector() throws Exception {
+        double[] x = new double[]{};
+        double[][] y = new double[][]{{}};
+        double[] testresult = new double[]{};
+        assertArrayEquals(testresult, ml.mult(y, x), 0);
 
     }
 
     @org.junit.Test
     public void testVectorMatrix() throws Exception {
+        double[] x = new double[]{3, 4, 2};
+        double[][] y = new double[][]{
+                {13, 9, 7, 15},
+                {8, 7, 4, 6},
+                {6, 4, 0, 3} };
+        double[] testresult = new double[]{83, 63, 37, 75};
+        assertArrayEquals(testresult, ml.mult(x, y), 0);
 
+
+    }
+
+
+    public boolean assertDoubleArrayEquals(double[][] expected, double[][] given) {
+        if (expected.length != given.length) return false;
+        try {
+            for (int i = 0; i < expected.length; i++) {
+                for (int j = 0; j < expected[i].length; j++){
+                    if (expected[i][j] != given[i][j]) {
+                        System.out.println("Double array not equal");
+                        return false;
+                    }
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("error validating assertArrayEquals[][]" + e);
+            return false;
+        }
+        System.out.println("assertDoubleArrayEquals passed successfully");
+        return true;
     }
 }

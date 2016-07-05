82ef8881210c07b8e69860b5dcbe7ba3c3dcd85e
bassijtsma <btsijtsma@gmail.com>
2016-07-05 23:19:40 +0200

    generate random points and test

--- algorithms/src/exercises/Point2D_1_2_1.java
+++ algorithms/src/exercises/Point2D_1_2_1.java
@@ -1,25 +1,37 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Write a point2D client thtat takes int value N and generates N random points in the unit square,
  * and compute the distance seperating the closest pair of points
  */
 public class Point2D_1_2_1 {
 
-    // array of points: [] ;
-
+    ArrayList<Point2D> points = new ArrayList<>();
+    final int ARBITRARYINTLIMIT = 9999999;
+    final int CLOSESTPOINTSXY = ARBITRARYINTLIMIT/2;
     public void generatePoints(int n) {
-
+        Random rand = new Random();
         for (int i = 0; i < Math.pow(n, 2); i ++) {
-//            result[i] = new point2d(random int random int);
+            points.add(new Point2D(rand.nextInt(ARBITRARYINTLIMIT), rand.nextInt(ARBITRARYINTLIMIT)));
         }
-//        array of points = result
     }
 
-    // calculate euclidian distance between points and find the ones that are closest
+    // Adds two points with the same x/y for test purposes
+    public void addClosePoints() {
+        Point2D firstpoint = new Point2D(CLOSESTPOINTSXY, CLOSESTPOINTSXY);
+        Point2D secondpoint = new Point2D(CLOSESTPOINTSXY, CLOSESTPOINTSXY);
+        points.add(firstpoint);
+        points.add(secondpoint);
+    }
+
+        // calculate euclidian distance between points and find the ones that are closest
     // probably good to do a hashmap of points and their distances
     // try to only do distance: a->b, and skip b->a;
-    public Point2D[] getClosestPoints() {
-
+    // returns x/y location of the two closest points
+    public ArrayList<Point2D> getClosestPoints() {
+        return points;
     }
 }
--- algorithms/src/exercises/Point2D_1_2_1Test.java
+++ algorithms/src/exercises/Point2D_1_2_1Test.java
@@ -8,6 +8,25 @@ import static org.junit.Assert.*;
 public class Point2D_1_2_1Test {
     Point2D_1_2_1 p2d = new Point2D_1_2_1();
 
+    @org.junit.Test
+    public void testGeneratePoints() {
+        p2d.generatePoints(10);
+        assertTrue("100 points created:", p2d.points.size() == 100 );
+        assertFalse("Not a 101 points created:", p2d.points.size() == 101 );
+
+        p2d.generatePoints(5);
+        assertTrue("100 points created:", p2d.points.size() == 125 );
+    }
+
+    @org.junit.Test
+    public void testGetClosestPoints() {
+        p2d.addClosePoints();
+        // what can we assert:
+        // that two closest points have distance 0
+        // that the two points returned are at location CLOSESTPOINTSXY
+        //
+
+    }
 
 
 }

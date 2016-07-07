8a38c09b27f61966f55dec25032c7d5717cb2990
bassijtsma <btsijtsma@gmail.com>
2016-07-07 19:31:52 +0200

    finished getclosestpoints and the test

--- algorithms/src/exercises/Point2D_1_2_1.java
+++ algorithms/src/exercises/Point2D_1_2_1.java
@@ -6,6 +6,7 @@ import java.util.Random;
 /**
  * Write a point2D client thtat takes int value N and generates N random points in the unit square,
  * and compute the distance seperating the closest pair of points
+ * Distance will be (double) euclidean distance
  */
 public class Point2D_1_2_1 {
 
@@ -18,26 +19,45 @@ public class Point2D_1_2_1 {
         Random rand = new Random();
         for (int i = 0; i < Math.pow(n, 2); i ++) {
             points.add(new Point2D(i, rand.nextInt(ARBITRARYINTLIMIT), rand.nextInt(ARBITRARYINTLIMIT)));
+
         }
     }
 
     // Adds two points with the same x/y for test purposes
     public void addClosePoints() {
         Point2D firstpoint = new Point2D(CLOSESTPOINTID, CLOSESTPOINTSXY, CLOSESTPOINTSXY);
-        Point2D secondpoint = new Point2D(CLOSESTPOINTID, CLOSESTPOINTSXY, CLOSESTPOINTSXY);
+        Point2D secondpoint = new Point2D(CLOSESTPOINTID, CLOSESTPOINTSXY+1, CLOSESTPOINTSXY+1);
         points.add(firstpoint);
         points.add(secondpoint);
     }
 
     // calculate euclidean distance between points and find the ones that are closest
-    // probably good to do a hashmap of points and their distances
-    // try to only do distance: a->b, and skip b->a;
-    // returns x/y location of the two closest points
-    public ArrayList<Point2D> getClosestPoints() {
-        for (Point2D point: points) {
-            System.out.println(point);
+    // returns the two closest points
+    // euclidean distance is defined as root( (qx-rx)^2 + (qy - ry)^2 )
+    public Point2D[] getClosestPoints() {
+        double closestDistance = ARBITRARYINTLIMIT;
+        Point2D[] closestPoints = new Point2D[2];
+
+        for (int i = 0; i < this.points.size(); i ++) {
+            for (int j = 0; j < this.points.size(); j++) {
+                // by checking if i >= j, we save half of all operations. Can be seen as matrix,
+                // where we can skip distance between a point itself ( = distance(a, a), and if
+                // distance(a, b) is calculated, we can skip distance(b, a)
+                if (i >= j ) continue;
+                double distance = getEuclideanDistance(points.get(i), points.get(j));
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+                    closestPoints[0] = points.get(i);
+                    closestPoints[1] = points.get(j);
+                }
+            }
         }
-        return points;
+        System.out.println("the closest: " + closestDistance);
+        System.out.println("the points are: " + closestPoints[0] + " " + closestPoints[1]);
+        return closestPoints;
     }
 
+    public double getEuclideanDistance(Point2D a, Point2D b) {
+        return Math.sqrt( Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2) );
+    }
 }
--- algorithms/src/exercises/Point2D_1_2_1Test.java
+++ algorithms/src/exercises/Point2D_1_2_1Test.java
@@ -20,14 +20,22 @@ public class Point2D_1_2_1Test {
 
     @org.junit.Test
     public void testGetClosestPoints() {
+        p2d.generatePoints(10);
         p2d.addClosePoints();
         // what can we assert:
         // that two closest points have distance 0
         // that the two points returned are at location CLOSESTPOINTSXY
         // that the closest points have an id of p2d.CLOSESTPOINTID
-        p2d.addClosePoints();
+        Point2D[] closestpoints = p2d.getClosestPoints();
+        System.out.println("after getting its:" + closestpoints[0]);
+        System.out.println("after getting its:" + closestpoints[1]);
 
-
+        assertTrue(closestpoints[0].getId() == p2d.CLOSESTPOINTID);
+        assertTrue(closestpoints[1].getId() == p2d.CLOSESTPOINTID);
+        assertTrue(closestpoints[0].getX() == p2d.CLOSESTPOINTSXY);
+        assertTrue(closestpoints[0].getY() == p2d.CLOSESTPOINTSXY);
+        assertTrue(closestpoints[1].getX() == p2d.CLOSESTPOINTSXY+1);
+        assertTrue(closestpoints[1].getY() == p2d.CLOSESTPOINTSXY+1);
 
     }
 

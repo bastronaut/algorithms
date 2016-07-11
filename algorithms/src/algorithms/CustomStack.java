a4969d71d3277a5b8c795899b1c5c92e5fb66a79
bassijtsma <btsijtsma@gmail.com>
2016-07-11 22:07:26 +0200

    added Node class to stack

--- algorithms/src/algorithms/CustomStack.java
+++ algorithms/src/algorithms/CustomStack.java
@@ -3,7 +3,7 @@ package algorithms;
 import java.util.EmptyStackException;
 
 /**
- * Implementation of a generic stack with a Linked List data structure
+ * Implementation of a generic stack with a single Linked List data structure
  * api is as follows:
  * push(): add to top of stack
  * pop() : remove from top of stack
@@ -15,20 +15,26 @@ public class CustomStack<T> implements Iterable<T>{
     private int N;
 
     public void push(T t) {
-
+        N++;
+        Node first;
+        Node last; // tricky
     }
 
-    // throw an emptystack exception when pop is called on an empty stack?
+    // What to do when pop() is called on an empty stack?
     // seems that consensus is that caller should check that the stack is not empty (!ismpety())
     // before calling the function
     // http://stackoverflow.com/questions/7390126/what-should-the-pop-method-return-when-the-stack-is-empty
     // http://stackoverflow.com/questions/23465239/what-exception-to-throw-for-pop-when-stack-is-empty
     public T pop() throws EmptyStackException {
-//        return T;
+        if (N == 0) {
+            throw new EmptyStackException();
+        }
+        N--;
+//        return
     }
 
     public boolean isEmpty() {
-
+        return N == 0;
     }
 
     public int size() {
@@ -40,11 +46,23 @@ public class CustomStack<T> implements Iterable<T>{
 //    }
 
     private class Node {
-        T next;
+        private Node next;
+        private T item;
 
-
-        public void setNext(T next) {
+        public void setNext(Node next) {
             this.next = next;
+        }
+
+        public void setItem(T item) {
+            this.item = item;
+        }
+
+        public T getItem() {
+            return this.item;
+        }
+
+        public Node getNext() {
+            return this.next;
         }
     }
 }

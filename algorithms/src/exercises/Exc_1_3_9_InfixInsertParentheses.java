5f6cc7e225f87be489a8ab83ac7ac03fa5f253b8
bassijtsma <btsijtsma@gmail.com>
2016-07-22 11:50:50 +0200

    added Exc 1.3.9 adding left parentheses to infix expression

---
+++ algorithms/src/exercises/Exc_1_3_9_InfixInsertParentheses.java
@@ -0,0 +1,44 @@
+package exercises;
+
+import algorithms.CustomStackLinkedList;
+
+/**
+ * Write a program that takes from standard input an expression without left
+ * parentheses and prints the equivalent inﬁx expression with the parentheses
+ * inserted. For example, given the input:
+ *   1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) ) your program should print ( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )
+ *
+ * solution: add numbers and operations to a seperate stack. whenever a ")" occurs, pop a number, operation and
+ * another number from the stack, and prefix it with a "(".
+ *
+ * Return the new prefixed expression on the stack of values as 1 value, and continue
+ *
+ */
+public class Exc_1_3_9_InfixInsertParentheses {
+    public String insertParentheses(String infixExpression) {
+        CustomStackLinkedList<String> operations = new CustomStackLinkedList<>();
+        CustomStackLinkedList<String> values = new CustomStackLinkedList<>();
+
+        String[] characters = infixExpression.split(" ");
+
+        for (String character : characters) {
+            if (character.equals("*") || character.equals("/") ||
+                character.equals("+") || character.equals("-")) {
+                operations.push(character);
+            } else if (character.equals(")")) {
+                String prefixedExpression = prefixExpressionWithBracket(values.pop(), operations.pop(), values.pop());
+                values.push(prefixedExpression);
+            } else {
+                values.push((character));
+            }
+        }
+
+        String returnValue = values.pop();
+        System.out.println(returnValue);
+        return returnValue;
+    }
+
+    private String prefixExpressionWithBracket(String secondValue, String operator, String firstValue) {
+        return "( "+ firstValue + " " + operator + " " + secondValue + " )";
+    }
+}
---
+++ algorithms/src/exercises/Exc_1_3_9_InfixInsertParenthesesTest.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Write a program that takes from standard input an expression without left
+ * parentheses and prints the equivalent inﬁx expression with the parentheses
+ * inserted. For example, given the input:
+ *   1 + 2 ) * 3 - 4 ) * 5 - 6 ) )  your program should print ( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) )
+ */
+public class Exc_1_3_9_InfixInsertParenthesesTest {
+
+    @org.junit.Test
+    public void testInsertParentheses() {
+        Exc_1_3_9_InfixInsertParentheses IIP = new Exc_1_3_9_InfixInsertParentheses();
+        String testOneInput = "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )";
+        String testOneOutput = "( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )";
+        assertTrue(IIP.insertParentheses(testOneInput).equals(testOneOutput));
+
+        assertTrue(IIP.insertParentheses("1 + 2 )").equals("( 1 + 2 )"));
+        assertTrue(IIP.insertParentheses("3 * 4 + 2 ) )").equals("( 3 * ( 4 + 2 ) )"));
+    }
+}

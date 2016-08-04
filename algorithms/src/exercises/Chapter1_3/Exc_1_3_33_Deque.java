767909787f8aaea5c8f28e6d87357852b7f14666
bastronaut <btsijtsma@gmail.com>
2016-08-04 12:21:01 +0100

    setup for deque implementation

---
+++ algorithms/src/exercises/Chapter1_3/Exc_1_3_33_Deque.java
@@ -0,0 +1,65 @@
+package exercises.Chapter1_3;
+
+/**
+ * 1.3.33 Deque. A double-ended queue or deque (pronounced
+ * “deck”) is like a stack or a queue but supports adding
+ * and removing items at both ends. A deque stores a
+ * collection of items and supports the following API:
+
+ Deque()
+ boolean isEmpty()
+ int size()
+ void pushLeft(Item item)
+ void pushRight(Item item)
+ Item popLeft()
+ item popRight()
+
+ Write a class  Deque that uses a doubly-linked list to
+ implement this API and a class ResizingArrayDeque that
+ uses a resizing array.
+
+
+ */
+public class Exc_1_3_33_Deque <Item> {
+
+    private int N;
+    private Node leftNode;
+    private Node rightNode;
+
+    public Exc_1_3_33_Deque () {
+        this.N = 0;
+        leftNode = null;
+        rightNode = null;
+    }
+
+    public void pushLeft(Item item) {
+
+    }
+
+    public void pushRight(Item item) {
+
+    }
+
+    public Item popLeft() {
+
+    }
+
+    public Item popRight() {
+
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+
+    private class Node {
+        Item item;
+        Node next;
+        Node previous;
+    }
+}
---
+++ algorithms/src/exercises/Chapter1_3/Exc_1_3_33_DequeTest.java
@@ -0,0 +1,31 @@
+package exercises.Chapter1_3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by BSijtsma on 02-08-2016.
+ */
+public class Exc_1_3_33_DequeTest {
+    @Test
+    public void pushLeft() throws Exception {
+        Exc_1_3_33_Deque<Integer> deque = new Exc_1_3_33_Deque<>();
+    }
+
+    @Test
+    public void pushRight() throws Exception {
+        Exc_1_3_33_Deque<Integer> deque = new Exc_1_3_33_Deque<>();
+    }
+
+    @Test
+    public void popLeft() throws Exception {
+        Exc_1_3_33_Deque<Integer> deque = new Exc_1_3_33_Deque<>();
+    }
+
+    @Test
+    public void popRight() throws Exception {
+        Exc_1_3_33_Deque<Integer> deque = new Exc_1_3_33_Deque<>();
+    }
+
+}

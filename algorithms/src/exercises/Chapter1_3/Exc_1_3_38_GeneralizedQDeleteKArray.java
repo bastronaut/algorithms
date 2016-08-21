b6e9d70a3eac809cb1372e5a40b51d0a8b4c550a
bassijtsma <btsijtsma@gmail.com>
2016-08-21 22:09:22 +0200

    setup for exc 1.3.38 GeneralizedQDeleteK

---
+++ algorithms/src/exercises/Chapter1_3/Exc_1_3_38_GeneralizedQDeleteKArray.java
@@ -0,0 +1,33 @@
+package exercises.Chapter1_3;
+
+/**
+ * 1.3.38  Delete kth element. Implement a class that supports the following API:
+ public class    GeneralizedQueue<Item>
+ GeneralizedQueue() create an empty queue
+ boolean isEmpty() is the queue empty?
+ void insert(Item x) add an item Item delete(int k) delete and return the kth least recently inserted item
+ First, develop an implementation that uses an array implementation, and then develop one
+ that uses a linked-list implementation.
+ Note : the algorithms and data structures that we introduce in Chapter 3 make it possible
+ to develop an implementation that can guarantee that both insert() and delete() take time
+ proportional to the logarithm of the number of items in the queue—see Exercise 3.5.27.
+
+
+ */
+public class Exc_1_3_38_GeneralizedQDeleteKArray implements Exc_1_3_38_GeneralizedQDeleteInterface{
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void insert(Object x) {
+
+    }
+
+    @Override
+    public Object delete(int k) {
+        return null;
+    }
+}

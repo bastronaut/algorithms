e39486c10f0ab50d4e257e29cd85c1aa2e3051b3
bastronaut <btsijtsma@gmail.com>
2016-09-03 11:41:02 +0100

    setup copyStack 41

---
+++ algorithms/src/exercises/Chapter1_3/Exc_1_3_42_CopyStack.java
@@ -0,0 +1,63 @@
+package exercises.Chapter1_3;
+
+/**
+ Copy a stack. Create a new constructor for the linked-list implementation of
+ Stack so that
+ Stack<Item> t = new Stack<Item>(s);
+ makes t a reference to a new and independent copy of the stack s
+ */
+public class Exc_1_3_42_CopyStack <Item> {
+
+    private Node topstackNode;
+    private int N;
+
+    public Exc_1_3_42_CopyStack() {
+
+    }
+
+    public Exc_1_3_42_CopyStack(Exc_1_3_42_CopyStack stackToCopy) {
+        // easiest way to copy a stack: use two stacks
+        while (!stackToCopy.isEmpty()) {
+
+        }
+
+    }
+
+    public Item pop() {
+        if (isEmpty()) {
+            throw new NullPointerException("stack is empty");
+        }
+        Item returnItem = topstackNode.item;
+        topstackNode = topstackNode.next;
+        N--;
+        return returnItem;
+    }
+
+    public void push(Item item) {
+        Node newNode = new Node();
+        newNode.item = item;
+        if (!isEmpty()) {
+            newNode.next = topstackNode;
+        }
+        topstackNode = newNode;
+        N++;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+
+        // will only be used for ints for excercise so thisll do
+        public String toString() {
+            return item.toString();
+        }
+    }
+}

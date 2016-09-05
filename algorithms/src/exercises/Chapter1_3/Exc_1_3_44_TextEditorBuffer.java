0266d4618d0c7c0ea3a25723260b722862746321
bassijtsma <btsijtsma@gmail.com>
2016-09-05 23:09:31 +0200

    setup 1.344 text editor buffer

---
+++ algorithms/src/exercises/Chapter1_3/Exc_1_3_44_TextEditorBuffer.java
@@ -0,0 +1,38 @@
+package exercises.Chapter1_3;
+
+/**
+ 1.3.44 Text editor buffer. Develop a data type for a buffer in a text
+ editor that implements the following API:
+
+ public class Buffer
+ Buffer() create an empty buffer
+ void insert(char c) insert c at the cursor position
+ char delete() delete and return the character at the cursor
+ void left(int k) move the cursor k positions to the left
+ void right(int k) move the cursor k positions to the right
+ int size() number of characters in the buffer
+ API for a text buffer
+ */
+public class Exc_1_3_44_TextEditorBuffer {
+
+    public void insert(char c) {
+
+    }
+
+    public char delete() {
+
+    }
+
+    public void left(int k) {
+
+    }
+
+    public void right(int k) {
+
+    }
+
+    public int size() {
+
+    }
+
+}

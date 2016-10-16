833a697812932043dd67fae88f83bb1b7f251729
bassijtsma <btsijtsma@gmail.com>
2016-10-16 21:50:47 +0200

    work on print sorted duplicates

--- algorithms/src/exercises/Chapter1_4/Exc_1_4_12_PrintSortedDuplicates.java
+++ algorithms/src/exercises/Chapter1_4/Exc_1_4_12_PrintSortedDuplicates.java
@@ -1,5 +1,7 @@
 package exercises.Chapter1_4;
 
+import java.util.ArrayList;
+
 /**
  1.4.12 Write a program that, given two sorted arrays of N int values, prints all elements
  that appear in both arrays, in sorted order. The running time of your program
@@ -15,8 +17,20 @@ public class Exc_1_4_12_PrintSortedDuplicates {
         this.secondArray = secondArray;
     }
 
+    // start by implementing a basic version then check the runtime
+    // if both arrays are sorted, you can look for the element in an array, and if its higher, stop searching
+    // should save run time. worst case would still be runtime N
     public String printSortedDoubles() {
-        return "";
+        ArrayList<Integer> sortedMatches = new ArrayList();
+        for (int x : firstArray) {
+            for (int y : secondArray) {
+                if (x > y) {
+                    break;
+                } else if (x == y) {
+//                    sortedMatches
+                }
+            }
+        }
     }
 }
 

b7b9b71dedde413594b53af0c6cc5c4a372bf859
bassijtsma <btsijtsma@gmail.com>
2016-10-17 22:59:20 +0200

    setup for quickunion

---
+++ algorithms/src/exercises/Chapter1_5/QuickUnion.java
@@ -0,0 +1,32 @@
+package exercises.Chapter1_5;
+
+/**
+ * Created by Bas on 16-10-2016.
+ *
+ * similar to quickfind in the data structure, except the values are
+ * interpreted differnetly: the values are 'links' to one another.
+ *
+ * Parent-> child link
+ *
+ */
+public class QuickUnion implements UnionFind {
+    @Override
+    public void union(int p, int q) {
+
+    }
+
+    @Override
+    public int find(int p) {
+        return 0;
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+}

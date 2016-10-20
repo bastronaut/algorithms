699db80cdbb67fbec8d0b2bb3b64a49c3b257b3f
bassijtsma <btsijtsma@gmail.com>
2016-10-20 23:25:09 +0200

    patch up testcase

--- algorithms/src/exercises/Chapter1_5/QuickFindTest.java
+++ algorithms/src/exercises/Chapter1_5/QuickFindTest.java
@@ -9,10 +9,11 @@ public class QuickFindTest {
 
     @org.junit.Test
     public void testQF() {
-        //test case:
         QuickFind qf = new QuickFind(10);
-        // already connected nodes: {8,9} , {1, 0}
+        // testcase: already connected nodes: {8,9} , {1, 0}
         int[][] pairs = new int[][]{{4, 3},{3, 8}, {6,5}, {9,4}, {2,1}, {8, 9},{5,0}, {7,2}, {6,1}, {1,0}, {6,7}};
+        int[][] groundTruth = new int[][]{{4, 3},{3, 8}, {6,5}, {9,4}, {2,1}, {5,0}, {7,2}, {6,1}};
+        int[][] testresult = new int[groundTruth.length][groundTruth[0].length];
         // the test: do not print out the pair if it is connected.
         for (int[] pair : pairs) {
             if (qf.isConnected(pair[0], pair[1])) {
@@ -20,8 +21,22 @@ public class QuickFindTest {
             } else {
                 qf.union(pair[0], pair[1]);
                 System.out.println(pair[0]+ " "+ pair[1]);
+                // todo add pairs to testresult
             }
         }
+        assertTrue(haveSamePairs(testresult, groundTruth));
     }
+
+    public boolean haveSamePairs(int[][] testresult, int[][] groundtruth) {
+        for (int i = 0; i < groundtruth.length; i++) {
+            for (int j = 0; j < groundtruth[i].length; j++) {
+                if (groundtruth[i][j] != testresult[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
 }
